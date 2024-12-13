7eaa9509ba922456b7b0b7554e420ef43f6d3a88
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-12-13 14:58:29 +0700

    Adding a Places Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -26,19 +27,10 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+    MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: ...,
-    );
-  }
+      home: const PlacesScreen(),
+    ),
+  );
 }
